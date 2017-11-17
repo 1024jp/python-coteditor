ac75022802f1833c3dec5686e0ca7cb253ca72e0
1024jp <1024jp@wolfrosch.com>
2017-11-17 10:22:30 +0900

    Rename “Check PEP8” to “Check Cod Style”

--- 05)Check Code Style.py
+++ 05)Check Code Style.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-"""PEP8 check script for CotEditor
+"""Python code style check script for CotEditor
 
-Check Python source code of the current document on CotEditor with pep8.
+Check Python source code of the current document on CotEditor with pycodestyle.
 This is a CotEditor script.
 """
 
@@ -19,16 +19,16 @@ from subprocess import Popen, PIPE
 # setting -----------------------------------------------------------
 
 # path to pycodestyle
-PEP8 = '/usr/local/bin/pycodestyle'
+command = '/usr/local/bin/pycodestyle'
 
 
 # main --------------------------------------------------------------
 
 def main(filepath):
     # check pep8
-    results = Popen([PEP8, filepath], stdout=PIPE).stdout
+    results = Popen([command, filepath], stdout=PIPE).stdout
 
-    # write results to CotEditor's Script Errors window
+    # write results to CotEditor's Console
     sys.stderr.write('checked -> ' + os.path.basename(filepath) + '\n')
     for line in results:
         sys.stderr.write(line.split(':', 1)[-1])
